144f3748f6d1becfb2de298d52720d3d45bd3453
lisethhomeofluxury <lisethhomeofluxury@gmail.com>
2024-12-09 10:28:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Javscript to match your HTML and CSS
+// Javscript for functionality
 
 document.addEventListener(DOMContentLoaded', () => {
   // Search Box Functionality
@@ -13,30 +13,31 @@ SearchBox.addEventListener( 'keypress', function (event) {
    } 
  });
 
-   // Buy Now Buttons Functionality
-   const buyNOwButtons = document.querySelectorAll('.buy-now');
-   buyNOwButtons.forEach((button => {
-    button.addEventListener('click', () => {
-        const productName = button.previousElementSibling.previousElementSibling.textContent;
-        const productPrice = button.previousElementSibling.textContent;
+   // Add functionality for "Buy Now" buttons
+   document.querySelectorAll('.buy-now').forEach((button) => {
+    button.addEventListener('click', (event) => {
+        const produceId = event.target.getAttribute('data-product-id');
+        alert('product with ID: &#8358{productId} added to cart!');
+        // Redirect to the cart page
+        window.localStorage.href = 'cart.html'
 
         alert('Added to cart: ${productName} for ${productPrice}');
         // Implement add-to-cart logic here
     });
 });
 
-// Cart Icon Functionality
-const cartIcon = document.querySelector('header .header-icon a:nth-child(2)');
-cartIcon.addEventListener('click', (event) => {
+// Cart logo Functionality
+const cartlogo = document.querySelector('header .header-icon a:nth-child(2)');
+cartlogo.addEventListener('click', (event) => {
     event.preventDefault();
     alert('Redirecting to the cart page...');
     // Implement cart page redirection
     window.location.href = 'cart.html';
   });
 
-   // User Icon Functionality
-   const userIcon = document.querySelector('header .header-icons a:nth-child(2)');
-   userIcon.addEventListener('click', (event) => {
+   // User id logo Functionality
+   const useridlogo = document.querySelector('header .header-icons a:nth-child(2)');
+   useridlogo.addEventListener('click', (event) => {
      event.preventDefault();
      alert('Redirecting to the login or user account page...');
      // Implement login/account page redirection
@@ -45,7 +46,8 @@ cartIcon.addEventListener('click', (event) => {
     
      // Social Media Links Functionality
      const socialMediaLinks = document.querySelectorAll('footer .social-media a');
-     socialMediaLinks.forEach((link, index) => {
+
+     socialMediaLinks.forEach((link, index) => { // Add index here
         link.addEventListener('click', (event) => {
             event.preventDefault ();
             switch (index) {
@@ -53,19 +55,19 @@ cartIcon.addEventListener('click', (event) => {
                     window.open('https://www.instagram.com/liseth_home_of_luxury?igsh=MTlrbzkwaWY5cjNtaQ%3D%3D', '_blank');
                     break;
                 case 1:
-                    window.open('https://www.facebook.com/share/1Ampmwyowk/?mibextid=LQQJ4d', '_blank');
+                    window.open('https://www.facebook.com/share/1Ampmwyowk/?mibextid=LQQJ4d', '_blank')
                     break;
                 case 2:
-                    window.open('https://snapchat.com/t/GtHtzsEV', '_blank');
+                    window.open('https://snapchat.com/t/GtHtzsEV', '_blank')
                     break
                 case 3:
-                    window.open('https://wa.me/message/J3FHVS2VFWD2L1', '_blank');
+                    window.open('https://wa.me/message/J3FHVS2VFWD2L1', '_blank')
                     break
                 default:
-                    alert('Social media link not configured.');
-            }
-        });
-    });
+                    alert('Redirecting to ${link.href}.')
+               }
+            });
+         });
 
     // Payment Methods Placeholder (Future Enhancement)
     // You can add functionality to dynamically display payment methods if required.
